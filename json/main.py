cce7bee430deeae0641a4cf2116354401a63643c
denismwangi <denniske992@gmail.com>
2020-12-10 13:00:46 +0300

    json &logging

---
+++ json/main.py
@@ -0,0 +1,9 @@
+import _json
+import json
+
+person = {"name": "John", "age": 30, "city": "New York", "hasChildren": False, "titles": ["engineer", "programmer"]}
+
+
+person_json = json.dumps()
+print(person_json)
+
